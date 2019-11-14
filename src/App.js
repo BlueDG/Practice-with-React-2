8240aa1644c002cc1383af82b06647aa5844c721
BlueDG <Guillaume.dusseuxgicquel@gmail.com>
2019-11-14 06:37:30 +0100

    rendu conditionnel

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ const team = {
 
 class App extends Component {
   state = {
-    team
+    team,
+    isShown: false
   };
 
   // METHOD 1
@@ -42,13 +43,6 @@ class App extends Component {
     }));
   }
 
-  // METHOD 1 again for practice
-  handleAgeSteven = () => {
-    const team = { ...this.state.team };
-    team.soldier3.age += 1;
-    this.setState({ team });
-  };
-
   handleName = event => {
     const team = { ...this.state.team };
     const name = event.target.value;
@@ -56,49 +50,51 @@ class App extends Component {
     this.setState({ team });
   };
 
-  handleNameSteve = event => {
-    const team = { ...this.state.team };
-    const name = event.target.value;
-    team.soldier3.name = name;
-    this.setState({ team });
+  handleShowWorker = () => {
+    const isShown = !this.state.isShown;
+    this.setState({ isShown });
   };
 
   render() {
-    const { team } = this.state;
+    const { team, isShown } = this.state;
+
+    let member = null;
+    if (isShown) {
+      member = <Soldier name={team.soldier3.name} age={team.soldier3.age} />;
+    }
+
+    const list = Object.keys(team).map(member => (
+      <Soldier name={team[member].name} age={team[member].age}>
+        Team member
+      </Soldier>
+    ));
+
     return (
       <>
         <div className="App">
-          <h1>Timor Crew</h1>
-          <Soldier name={team.soldier1.name} age={team.soldier1.age}>
-            Team administrator
-          </Soldier>
-          <input
-            type="text"
-            value={team.soldier1.name}
-            onChange={this.handleName}
-          />
-          <Soldier name={team.soldier2.name} age={team.soldier2.age} />
-          <Soldier name={team.soldier3.name} age={team.soldier3.age} />
-          <input
-            type="text"
-            value={team.soldier3.name}
-            onChange={this.handleNameSteve}
-          />
-        </div>
-        <div className="button-container">
-          <button className="button" onClick={this.handleAgeBis}>
-            Add +1 year Admin
+          <h1>Team</h1>
+          {list}
+          {member}
+          <button className="button" onClick={this.handleShowWorker}>
+            {isShown ? "Hide Member" : "Show Member"}
           </button>
-        </div>
-        <div className="button-container">
-          <button className="button" onClick={() => this.handleAge(+1)}>
-            Add +1 year Charles
-          </button>
-        </div>
-        <div className="button-container">
-          <button className="button" onClick={this.handleAgeSteven}>
-            Add +1 year Steven
-          </button>
+          <div className="button-container">
+            <button className="button" onClick={this.handleAgeBis}>
+              Add +1 year Admin
+            </button>
+          </div>
+          <div className="button-container">
+            <button className="button" onClick={() => this.handleAge(+1)}>
+              Add +1 year Charles
+            </button>
+          </div>
+          <div>
+            <input
+              type="text"
+              value={team.soldier1.name}
+              onChange={this.handleName}
+            />
+          </div>
         </div>
       </>
     );
